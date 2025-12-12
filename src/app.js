75cf26d334058d945b92b34624ea445b107689a9
Orne <killu@gmail.com>
2025-12-11 21:21:59 -0300

    initial steps

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
 import express from "express";
-import MongoStore from "connect-mongo";
 import mongoose from "mongoose";
+import usersRouter from "./routes/userRouter.js";
 
 const app = express();
 const PORT = 8080;
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Iniciamos la conexión con MongoDB
+const uri = "mongodb://127.0.0.1:27017/class-zero";
+mongoose.connect(uri);
 
-import mongoose from "mongoose";
-mongoose.connect("mongodb://localhost:27017/mydatabase");
+// Middlewares incorporados de Express
+app.use(express.json()); // Formatea los cuerpos json de peticiones entrantes.
+app.use(express.urlencoded({ extended: true })); // Formatea query params de URLs para peticiones entrantes.
+
+app.use("/api/users", usersRouter);
 
 try {
-  httpServer.listen(PORT, () => {
-    console.log(`🚀 ~ express.listen ~ servidor corriendo en el puerto PORT`);
+  app.listen(PORT, () => {
+    console.log(
+      `🚀 ~ express.listen ~ servidor corriendo en el puerto ${PORT}`
+    );
   });
 } catch (error) {
   console.error("Error al iniciar el servidor:", error);
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,3 +1,14 @@
 import { Router } from "express";
+import userModel from "../models/userModel.js";
 
 const router = Router();
+
+// Consultar todos los usuarios
+
+// Crear un usuario
+
+// Actualizar un usuario
+
+// Eliminar un usuario
+
+export default router;
