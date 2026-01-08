7caa10a4d7538b71ec3d09cb2772f8269b44e952
Orne <killu@gmail.com>
2026-01-07 22:31:05 -0300

    adding auth

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -4,4 +4,38 @@ import GitHubStrategy from "passport-github2";
 import userService from "../models/user.js";
 import { createHash, isValidPassword } from "../utils.js";
 
-const LocalStrategy = local.Strategy;
+// const LocalStrategy = local.Strategy;
+
+const initializePassport = () => {
+  passport.use(
+    "github",
+    new GitHubStrategy({
+      clientID: "",
+      clientSecret: "",
+      callbackURL: "",
+    }),
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        console.log(profile);
+        let user = await userService.findOne({ email: profile._json.email });
+        if (!user) {
+          //El usuario no existia en el sitio web, se lo agrega a la base de datos.
+          let newUser = {
+            first_name: profile._json.name,
+            last_name: "",
+            age: 18,
+            email: profile._json.email,
+            password: "",
+          };
+          let result = await userService.create(newUser);
+          done(null, result);
+        } else {
+          //El usuario ya existía
+          done(null, user);
+        }
+      } catch (error) {
+        return done(error);
+      }
+    }
+  );
+};
--- src/utils.js
+++ src/utils.js
@@ -5,5 +5,5 @@ export const createHash = (password) =>
   bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
 //Validar contraseña
-export const isValidPassword = (userModel, password) =>
+export const isValidPassword = (user, password) =>
   bcrypt.compareSync(password, user.password);
