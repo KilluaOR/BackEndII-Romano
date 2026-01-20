d1967c9b8cdb9911fdf6e2605fb678635a3a45c1
Orne <killu@gmail.com>
2026-01-20 19:47:07 -0300

    Agregando sessions router y controllers

---
+++ src/controllers/session.controllers.js
@@ -0,0 +1,35 @@
+import passport from "passport"
+
+//Controlador de registro
+export const registerController = passport.authenticate("register", {
+    failureRedirect: "/login?error=El usuario ya existe",
+    successRedirect: "/login?succes=Usuario registrado correctamente",
+});
+
+//Controlador de login
+export const loginController = passport.authenticate("login", {
+    failureRedirect: "/login?error=Credenciales incorrectas",
+    successRedirect: "/current",
+});
+
+//Controlador para iniciar autenticación con GitHub
+export const githubController = passport.authenticate("github", {
+    scope: ["user:email"],
+})
+
+//Controlador callback de GitHub
+export const githubCallbackController = passport.authenticate("github", {
+    failureRedirect: "/login?error=Error al autenticar con GitHub",
+    successRedirect: "/current",
+});
+
+//Controlador logout
+export const logoutController = (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(500).json({ error: "Error al cerrar sesión"})
+        }
+        res.clearCookie("connect.sid");
+        res.redirect("/login");
+    })
+}
--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -1,3 +1,22 @@
 import { Router } from "express";
-import passport from "passport";
+import { githubCallbackController, githubController, loginController, logoutController, registerController } from "../controllers/session.controllers";
+
+
 const router = Router();
+
+//Ruta de registro
+router.post("/register", registerController);
+
+//Ruta de login
+router.post("/login", loginController);
+
+//Ruta para iniciar autenticación con GitHub
+router.get("/github", githubController);
+
+//Callback de GitHub
+router.get("/githubcallback", githubCallbackController);
+
+//Ruta del logout
+router.post("/logout", logoutController);
+
+export default router;
